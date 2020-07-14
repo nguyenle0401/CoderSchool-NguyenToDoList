f9e426d07c03969b2cda400cb11f2c96776e4df1
Nguyen Le <nle20959@gmail.com>
2020-07-15 00:47:26 +0700

    2nd commit

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
 let todoList = [];
 
-const addTodo = () => {
-  let todo = document.getElementById("todoInput").value;
-
-  let itemTodo = { contents: todo, complete: false };
-
-  todoList.push(itemTodo);
-  console.log(todoList);
-  render();
-};
-
-const toggleDone = (index) => {
-  todoList[index].complete = true;
-  console.log(todoList);
-  render();
-};
-
 const render = () => {
-  let todoHTML = todoList
-    .map((item, index) => {
-      if (item.complete == false) {
-        return `<div class="item-style">Todo:${item.contents} <a onclick="toggleDone(${index})" href="#">Done</a></div>`;
-      } else {
-        return `<div class="item-style">Todo:${item.contents} <a onclick="toggleDone(${index})" href="#">UnDone</a></div>`;
-      }
+    let todoHTML = todoList.map((item, index) => {
+        if (item.done == true) {
+            return `<div class="item-style"><input type="checkbox" onClick="checkboxClicked(${index})" checked>${item.contents} <div onClick="removeClicked()"><i class="far fa-times-circle" style ="color:red" ></i></div></div>`;
+        } else {
+            return `<div class="item-style"><input type="checkbox" onClick="checkboxClicked(${index})" >${item.contents} <div onClick="removeClicked()"><i class="far fa-times-circle" style ="color:red"></i></div></div>`;
+        }
     })
-    .join("");
-  document.getElementById("resultArea").innerHTML = todoHTML;
+        .join("");
+    document.getElementById("resultArea").innerHTML = todoHTML;
 };
+
+
+const checkboxClicked = (index) => {
+    todoList[index].done = !todoList[index].done;
+    console.log(todoList);
+    render();
+};
+const removeClicked = (index) =>{
+    todoList.splice(index,1)
+    render();
+}
+
+const addTodoItemClicked = () => {
+    let todoValue = document.getElementById("todoInput").value;
+
+    let todoItem = {
+        contents: todoValue,
+        done: false
+    };
+
+    todoList.push(todoItem);
+    console.log(todoList);
+    render();
+};
+const allBtnClicked = (index) =>{
+    render();
+}
+const doneBtnClicked = (index) =>{
+    todoList.splice(index,1)
+    render();
+}
+const undoneBtnClicked = (index) =>{
+    todoList.splice(index,1)
+    render();
+}
+
+
+
+
