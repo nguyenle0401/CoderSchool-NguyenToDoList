69b83c2976b1879efa9ff93aaf97fe6e4ad939a0
Nguyen Le <nle20959@gmail.com>
2020-07-15 08:11:32 +0700

    Final

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 let todoList = [];
+window.onload = window.onbeforeunload = () => {
+    let lastTodoList = localStorage.getItem("todolist");
+    if(lastTodoList){
+        todoList = JSON.parse(lastTodoList);
+    }
+    render();
+}
 
 const render = () => {
+    localStorage.setItem('todolist', JSON.stringify(todoList));
+
     let todoHTML = todoList.map((item, index) => {
-        if (item.done == true) {
-            return `<div class="item-style"><input type="checkbox" onClick="checkboxClicked(${index})" checked>${item.contents} <div onClick="removeClicked()"><i class="far fa-times-circle" style ="color:red" ></i></div></div>`;
-        } else {
-            return `<div class="item-style"><input type="checkbox" onClick="checkboxClicked(${index})" >${item.contents} <div onClick="removeClicked()"><i class="far fa-times-circle" style ="color:red"></i></div></div>`;
+        if(item.state){
+            if (item.done == true) {
+                return `<div class="item-style strick-through"><input type="checkbox" onClick="checkboxClicked(${index})" checked>${item.contents} <div onClick="removeClicked(${index})"><i class="far fa-times-circle" style ="color:red" ></i></div></div>`;
+            } else {
+                return `<div class="item-style"><input type="checkbox" onClick="checkboxClicked(${index})" >${item.contents} <div onClick="removeClicked(${index})"><i class="far fa-times-circle" style ="color:red"></i></div></div>`;
+            }
         }
     })
         .join("");
@@ -25,27 +36,48 @@ const removeClicked = (index) =>{
 
 const addTodoItemClicked = () => {
     let todoValue = document.getElementById("todoInput").value;
-
     let todoItem = {
         contents: todoValue,
-        done: false
+        done: false,
+        state: true //true : show, false: hide
     };
     todoList.push(todoItem);
     console.log(todoList);
+    document.getElementById("todoInput").value = '';
     render();
 };
 
-// const allBtnClicked = (index) =>{
-//     render();
-// }
-// const doneBtnClicked = (index) =>{
-//     todoList.splice(index,1)
-//     render();
-// }
-// const undoneBtnClicked = (index) =>{
-//     todoList.splice(index,1)
-//     render();
-// }
+const allBtnClicked = () =>{
+    todoList.forEach(item => {
+        item.state = true;
+    });
+    render();
+}
+
+const doneBtnClicked = () =>{
+    todoList.forEach((item, index) => {
+        console.log(index)
+        if(item.done){
+            todoList[index].state = true;
+        }else{
+            todoList[index].state = false;
+        }
+    })
+    
+    render();
+}
+
+const undoneBtnClicked = () =>{
+    todoList.forEach((item,index) => {
+        if(item.done){
+            todoList[index].state = false;
+        }else{
+            todoList[index].state = true;
+        }
+    })
+    
+    render();
+}
 
 
 
